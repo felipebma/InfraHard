2d033171242d91e7b7e87e584789ad870ca6b9c5
César Silva <accs2@cin.ufpe.br>
2019-05-16 20:55:07 -0300

    Arrumando SB e SH

--- Final Project 1.0/StoreControl.v
+++ Final Project 1.0/StoreControl.v
@@ -1,14 +1,20 @@
-module StoreControl(Input,Output,Sinal);
+module StoreControl(Input,MemData,Output,Sinal);
 
 input wire [1:0] Sinal;
-input wire [31:0] Input;
+input wire [31:0] Input,MemData;
 output reg [31:0] Output;
 
 always @(*) begin
 	case(Sinal)
 		2'b00: Output <= Input;
-		2'b01: Output[15:0] <= Input[15:0];
-		2'b10: Output[7:0] <= Input[7:0];
+		2'b01: begin
+			Output[15:0] <= Input[15:0];
+			Output[31:16] <= MemData[31:16];
+		end
+		2'b10: begin
+			Output[7:0] <= Input[7:0];
+			Output[31:8] <= MemData[31:8];
+		end
 	endcase
 end
 
